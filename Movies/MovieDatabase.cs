94e4ff317055849460ae2b21a5104acb0f34659c
akdreesen <akdreesen@CIS-RDS3>
2020-04-22 19:50:53 -0500

    NO SERVER SWITCH

--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -20,6 +20,54 @@ namespace Movies
         public static IEnumerable<Movie> All { get { return movies; } }
 
         /// <summary>
+        /// Gets the possible MPAARatings
+        /// </summary>
+        public static string[] MPAARatings
+        {
+            get => new string[]
+            {
+            "G",
+            "PG",
+            "PG-13",
+            "R",
+            "NC-17"
+            };
+        }
+
+        /// <summary>
+        /// Loads the movie database from the JSON file
+        /// </summary>
+        static MovieDatabase()
+        {
+
+            using (StreamReader file = System.IO.File.OpenText("movies.json"))
+            {
+                string json = file.ReadToEnd();
+                movies = JsonConvert.DeserializeObject<List<Movie>>(json);
+            }
+            HashSet<string> genreSet = new HashSet<string>();
+            foreach (Movie movie in movies)
+            {
+                if (movie.MajorGenre != null)
+                {
+                    genreSet.Add(movie.MajorGenre);
+                }
+            }
+            genres = genreSet.ToArray();
+
+        }
+
+
+        // The genres represented in the database
+        private static string[] genres;
+
+        /// <summary>
+        /// Gets the movie genres represented in the database 
+        /// </summary>
+        public static string[] Genres => genres;
+
+
+        /// <summary>
         /// Searches the database for matching movies
         /// </summary>
         /// <param name="terms">The terms to search for</param>
@@ -39,34 +87,6 @@ namespace Movies
                 }
             }
             return results;
-        }
-
-        /// <summary>
-        /// Loads the movie database from the JSON file
-        /// </summary>
-        static MovieDatabase() {
-            
-            using (StreamReader file = System.IO.File.OpenText("movies.json"))
-            {
-                string json = file.ReadToEnd();
-                movies = JsonConvert.DeserializeObject<List<Movie>>(json);
-            }
-        }
-
-
-        /// <summary>
-        /// Gets the possible MPAARatings
-        /// </summary>
-        public static string[] MPAARatings
-        {
-            get => new string[]
-            {
-            "G",
-            "PG",
-            "PG-13",
-            "R",
-            "NC-17"
-            };
         }
 
 
@@ -92,6 +112,81 @@ namespace Movies
             }
             return results;
         }
+
+
+        /// <summary>
+        /// Filters the provided collection of movies
+        /// </summary>
+        /// <param name="movies">The collection of movies to filter</param>
+        /// <param name="genres">The genres to include</param>
+        /// <returns>A collection containing only movies that match the filter</returns>
+        public static IEnumerable<Movie> FilterByGenre(IEnumerable<Movie> movies, IEnumerable<string> genres)
+        {
+            // TODO: Filter the list
+            // If no filter is specified, just return the provided collection
+            if (genres == null || genres.Count() == 0) return movies;
+            // Filter the supplied collection of movies
+            List<Movie> results = new List<Movie>();
+            foreach (Movie movie in movies)
+            {
+                if (movie.MajorGenre != null && genres.Contains(movie.MajorGenre))
+                {
+                    results.Add(movie);
+                }
+            }
+            return results;
+        }
+
+
+
+        /// <summary>
+        /// Filters the provided collection of movies 
+        /// to those with IMDB ratings falling within 
+        /// the specified range
+        /// </summary>
+        /// <param name="movies">The collection of movies to filter</param>
+        /// <param name="min">The minimum range value</param>
+        /// <param name="max">The maximum range value</param>
+        /// <returns>The filtered movie collection</returns>
+        public static IEnumerable<Movie> FilterByIMDBRating(IEnumerable<Movie> movies, double? min, double? max)
+        {
+            // TODO: Filter movies
+            if (min == null && max == null) return movies;
+
+            var results = new List<Movie>();
+
+            // only a maximum specified
+            if (min == null)
+            {
+                foreach (Movie movie in movies)
+                {
+                    if (movie.IMDBRating <= max) results.Add(movie);
+                }
+                return results;
+            }
+
+            // only a minimum specified 
+            if (max == null)
+            {
+                foreach (Movie movie in movies)
+                {
+                    if (movie.IMDBRating >= min) results.Add(movie);
+                }
+                return results;
+            }
+
+            // Both minimum and maximum specified
+            foreach (Movie movie in movies)
+            {
+                if (movie.IMDBRating >= min && movie.IMDBRating <= max)
+                {
+                    results.Add(movie);
+                }
+            }
+            return results;
+
+        }
+
 
     }
 }
--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -17,22 +17,42 @@ namespace Movies.Pages
         /// <summary>
         /// The current search terms 
         /// </summary>
-        public string SearchTerms { get; set; }
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
 
         /// <summary>
         /// The filtered MPAA Ratings
         /// </summary>
+        [BindProperty]
         public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// The filtered genres
+        /// </summary>
+        [BindProperty]
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// The minimum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// The maximum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMax { get; set; }
 
         /// <summary>
         /// Gets the search results for display on the page
         /// </summary>
         public void OnGet()
         {
-            SearchTerms = Request.Query["SearchTerms"];
-            MPAARatings = Request.Query["MPAARatings"];
             Movies = MovieDatabase.Search(SearchTerms);
             Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
         }
 
     }
